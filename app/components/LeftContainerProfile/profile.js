a723841e03e505874537bbff458a3ddfe4cc5835
Gmes23 <gmesda23@gmail.com>
2017-03-06 21:45:04 -0500

    finish profile side container on left side nav

--- app/components/LeftContainerProfile/profile.js
+++ app/components/LeftContainerProfile/profile.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import Img from './Img';
+import Profile from './profile.png'
+
 const Container = styled.div`
   height: 40%;
   width: 145px;
@@ -15,11 +18,70 @@ const Userimg = styled.div`
   background-image: url(./profile.png);
 `;
 
+const Username_container = styled.div`
+  width: 100%;
+  height: 45px;
+`;
+
+const Welcome = styled.p`
+  font-family: museo-sans, sans-serif;
+  font-style: normal;
+  font-weight: 100;
+  margin: 0px;
+  color: rgb(191,191,191);
+  margin-top: 15px;
+`;
+
+const User_name = styled.p`
+  font-family: tablet-gothic, sans-serif;
+  font-style: normal;
+  font-weight: 600;
+  margin: 0px;
+`;
+
+const Span = styled.span`
+  position: absolute;
+  border-bottom: solid 2px #fe0000;
+  width: 15px;
+  left: 3.8%;
+`;
+
+const Profile_icon = styled.div`
+  width:100%;
+  margin-top: 10px
+  vertical-align: text-bottom;
+`;
+
+const My_profile = styled.p`
+  font-family: museo-sans, sans-serif;
+  font-style: normal;
+  font-weight: 100;
+  margin: 0px;
+  font-size: 13px;
+  color: rgb(191,191,191);
+`;
+
+const Test = styled.a`
+  vertical-align: text-bottom;
+`;
+
 class ProfileContainer extends Component {
   render() {
     return (
         <Container>
-            <Userimg />
+            <Userimg>
+              <Img src={Profile} alt="something" />
+            </Userimg>
+            <Username_container>
+                <Welcome>Welcome,</Welcome>
+                {/*max charcters allowed*/}
+                <User_name> John Trevskye </User_name>
+                <Span />
+            </Username_container>
+            <Profile_icon>
+                <Test  className="material-icons">person</Test >
+                <My_profile> My Profile </My_profile>
+            </Profile_icon>
         </Container>
         );
   }
