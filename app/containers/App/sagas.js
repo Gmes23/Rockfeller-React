748276fef3667413f14683abb04dcf47434b3c53
Gmes23 <gmesda23@gmail.com>
2017-03-11 21:39:27 -0500

    added sagas for app container

--- app/containers/App/sagas.js
+++ app/containers/App/sagas.js
@@ -1,36 +1,47 @@
-import { take, put, call } from 'redux-saga/effects';
+/**
+ * Gets the repositories of the user from Github
+ */
 
-function fetchData() {
-    return  fetch(url)
-    .then(res => res.json() )
-    .then(data => ({ data }) )
-    .catch(ex => {
-        console.log('parsing failed', ex);
-        return ({ ex });
-    });
+import { take, call, put, select, cancel, takeLatest } from 'redux-saga/effects';
+import { LOCATION_CHANGE } from 'react-router-redux';
+import { LOAD_REPOS } from 'containers/App/constants';
+import { reposLoaded, repoLoadingError } from 'containers/App/actions';
+
+import request from 'utils/request';
+import { makeSelectUsername } from 'containers/HomePage/selectors';
+
+/**
+ * Github repos request/response handler
+ */
+export function* getRepos() {
+  // Select username from store
+  const username = yield select(makeSelectUsername());
+  const requestURL = `https://api.github.com/users/${username}/repos?type=all&sort=updated`;
+
+  try {
+    // Call our request helper (see 'utils/request')
+    const repos = yield call(request, requestURL);
+    yield put(reposLoaded(repos, username));
+  } catch (err) {
+    yield put(repoLoadingError(err));
+  }
 }
 
-function* yourSaga(action) {
-    const { data, ex } = yield call(fetchData);
-    if (data)
-    yield put({ type: 'REQUEST_DONE', data });
-    else
-    yield put({ type: 'REQUEST_FAILED', ex });
-}
-export default function* watchAsync() {
-    yield* takeLatest('BLAH', yourSaga);
-}
+/**
+ * Root saga manages watcher lifecycle
+ */
+export function* githubData() {
+  // Watches for LOAD_REPOS actions and calls getRepos when one comes in.
+  // By using `takeLatest` only the result of the latest API call is applied.
+  // It returns task descriptor (just like fork) so we can continue execution
+  const watcher = yield takeLatest(LOAD_REPOS, getRepos);
 
-class App extends Component {
-    componentWillMount() {
-        this.props.dispatch({type: 'BLAH'});
-    }
-
-    render(){
-        return (<div>Data: {this.props.data}</div>);
-    }
+  // Suspend execution until location changes
+  yield take(LOCATION_CHANGE);
+  yield cancel(watcher);
 }
 
-export default connect( state =>({
-    data:state.data
-}))(App);
+// Bootstrap sagas
+export default [
+  githubData,
+];
