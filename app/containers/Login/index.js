e9921cdc4b1e1f2891b28d0cb85c98e253207aee
Gmes23 <gmesda23@gmail.com>
2017-03-23 20:01:00 -0400

    changes to register form/redux thunk test

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -3,24 +3,24 @@ import React from 'react';
 // import { createAndShow, LOCK_CONTAINER_ID } from './lib';
 // import styles from './styles.css';
 
-export class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
-  static propTypes = {
-    location: React.PropTypes.shape({
-      state: React.PropTypes.shape({
-        nextPathname: React.PropTypes.string.isRequired,
-      }),
-    }).isRequired,
-  };
+// export class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+//   static propTypes = {
+//     location: React.PropTypes.shape({
+//       state: React.PropTypes.shape({
+//         nextPathname: React.PropTypes.string.isRequired,
+//       }),
+//     }).isRequired,
+//   };
 
-  componentDidMount() {
-    const nextPathname = this.props.location.state && this.props.location.state.nextPathname;
-    createAndShow(nextPathname || '/');
-  }
+//   componentDidMount() {
+//     const nextPathname = this.props.location.state && this.props.location.state.nextPathname;
+//     createAndShow(nextPathname || '/');
+//   }
 
-  render() {
-    return <div id={LOCK_CONTAINER_ID} className={styles.container} />;
-    return <div> you are logged in </div>
-  }
-}
+//   render() {
+//     return <div id={LOCK_CONTAINER_ID} className={styles.container} />;
+//     return <div> you are logged in </div>
+//   }
+// }
 
-export default Login;
+// export default Login;
--- app/containers/SignUp/index.js
+++ app/containers/SignUp/index.js
@@ -29,3 +29,96 @@ const Button = styled.button`
 
 `;
 
+
+class SignupForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            userscreenname: '',
+            email: '',
+            password: '',
+            passwordConfirmation: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        axios.post('/api/users')
+    }
+
+    render() {
+        return (
+            <Form onSubmit={this.onSubmit} >
+                <h1> Register </h1>
+                <TextFieldGroup
+                  type="text"
+                  placeholder="username"
+                  value={this.state.userscreenname}
+                  onChange={this.onChange}
+                  name="userscreenname"
+                 />
+
+                  <TextFieldGroup
+                  type="text"
+                  placeholder="password"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  name="userscreenname"
+                 />
+
+                  <TextFieldGroup
+                  type="text"
+                  placeholder="email"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  name="userscreenname"
+                 />
+
+                    <TextFieldGroup
+                  type="text"
+                  placeholder="email"
+                  value={this.state.passwordConfirmation}
+                  onChange={this.onChange}
+                  name="userscreenname"
+                 />
+
+                 <Button> REGISTER </Button>
+
+            </Form>
+        )
+    }
+}
+
+
+export function* loginSaga() {
+  while(true) {
+    const { user, pass } = yield take(LOGIN_REQUEST)
+    try {
+      let { data } = yield call(request.post, '/login', { user, pass });
+      yield fork(loadUserData, data.uid);
+      yield put({ type: LOGIN_SUCCESS, data });
+    } catch(error) {
+      yield put({ type: LOGIN_ERROR, error });
+    }  
+  }
+}
+
+export function* loadUserData(uid) {
+  try {
+    yield put({ type: USERDATA_REQUEST });
+    let { data } = yield call(request.get, `/users/${uid}`);
+    yield put({ type: USERDATA_SUCCESS, data });
+  } catch(error) {
+    yield put({ type: USERDATA_ERROR, error });
+  }
+}
+
+
+export default SignupForm;
